c71b78e53177fe362825df36f573282b60696cb4
Vishal Balaji Sivaraman <64977199+The-SocialLion@users.noreply.github.com>
2020-09-17 21:27:47 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'electronics-calc',         # How you named your package folder (MyLib)
   packages = ['electronics-calc'],   # Chose the same as "name"
-  version = '1.0',      # Start with a small number and increase it with every change you make
+  version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'this is a package houses various steps templates which can be used to calculate values for problems involving desighn of circuits , verification of circuits and so on',   # Give a short description about your library
   author = 'Vishal Balaji Sivaraman',                   # Type in your name
