149eb782978353c1139498ff4c9161e3eedeb1bf
Vishal Balaji Sivaraman <64977199+The-SocialLion@users.noreply.github.com>
2020-09-17 19:47:07 +0530

    included setup file

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from distutils.core import setup
 setup(
-  name = 'YOURPACKAGENAME',         # How you named your package folder (MyLib)
-  packages = ['YOURPACKAGENAME'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  name = 'Electronics-calc',         # How you named your package folder (MyLib)
+  packages = ['Electronics-calc'],   # Chose the same as "name"
+  version = '1.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
-  author = 'YOUR NAME',                   # Type in your name
-  author_email = 'your.email@domain.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  author = 'Vishal Balaji Sivaraman',                   # Type in your name
+  author_email = 'vb.sivaraman_official@yahoo.com',      # Type in your E-Mail
+  url = 'https://github.com/The-SocialLion/Electronics-calc',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/The-SocialLion/Electronics-calc/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['electronics', 'calculator', 'Electronics-calculations','desighn'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'numpy',
+          'pandas',
+          'mathplotlib'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
