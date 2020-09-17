b1e7405d8ec77a6ef403f802104ff746c0c2eb70
Vishal Balaji Sivaraman <64977199+The-SocialLion@users.noreply.github.com>
2020-09-17 19:50:10 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   packages = ['Electronics-calc'],   # Chose the same as "name"
   version = '1.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'this is a package houses various steps/templates which can be used to calculate values for problems involving desighn of circuits , verification of circuits and so on',   # Give a short description about your library
+  description = 'this is a package houses various steps templates which can be used to calculate values for problems involving desighn of circuits , verification of circuits and so on',   # Give a short description about your library
   author = 'Vishal Balaji Sivaraman',                   # Type in your name
   author_email = 'vb.sivaraman_official@yahoo.com',      # Type in your E-Mail
   url = 'https://github.com/The-SocialLion/Electronics-calc',   # Provide either the link to your github or to your website
