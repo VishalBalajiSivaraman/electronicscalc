1dee8af142400688cc974500085faa57dfa9e4fc
Vishal Balaji Sivaraman <64977199+The-SocialLion@users.noreply.github.com>
2020-09-17 20:09:07 +0530

    Update __init__.py

--- electronics-calc/__init__.py
+++ electronics-calc/__init__.py
@@ -51,7 +51,7 @@ def gain_calc(Vo,Vi):
   dataset = pd.DataFrame({'Vo(Volts)': data[0,:], 'Gain': data[1,:], 'Gain(DB)': data[2,:]})
   print(dataset)
 
-def desighn_procedure(Rc,B,c,d):
+def design_procedure(Rc,B,c,d):
   Vcc=12
   Ve=1
   C1=0.1
